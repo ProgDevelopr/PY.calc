67f466a30777984d35dd8a72aa438d6a86ef8b41
prog202 <meteturan0706@gmail.com>
2025-07-07 13:41:58 +0300

    Cube, LCM and GCD

--- calculator.py
+++ calculator.py
@@ -4,14 +4,14 @@ import math as mt
 try:
     print("Made by PyDev")
     
-    print("+ = Addition\n- = Subtraction\n* = Multiplication\n/ = Division\n//% = Floor division with remainder\n** = Exponents\n% = Percentages")
-    print("%2 = Even or odd\n-- = Subtraction with negative numbers\nSR = Square and square root\nO = Rounding")
-    print("+++ = Artihmetic addition\n~ = Median\n! = Factorial\nABS = Absolute value\nCA = Circle Area")
-    print("Cir = Circumference of the circle\n")
+    print("+ = Addition\n- = Subtraction\n* = Multiplication\n/ = Division\n//% = Floor division with remainder\n** = Exponents")
+    print("% = Percentages\n%2 = Even or odd\n-- = Subtraction with negative numbers\nSR = Square and square root")
+    print("CR = Cube and cube root\nO = Rounding\n+++ = Artihmetic addition\n~ = Median\n! = Factorial\nABS = Absolute value")
+    print("CA = Circle Area\nCir = Circumference of the circle\nLCM = Least common multiple\nGCD = Greatest common divisor")
     
-    a = input("Please select a symbol: (+,-,*,/,//%,**,%,%2,--,SR,O,+++,~,!,ABS,CA,Cir) ")
+    a = input("Please select a symbol: (+,-,*,/,//%,**,%,%2,--,SR,CR,O,+++,~,!,ABS,CA,Cir,LCM,GCD) ")
     
-    if a!="%2" and a.upper()!="SR" and a.upper()!="O" and a!="+++" and a!="~" and a!="!" and a.upper()!="ABS" and a.upper()!="CA" and a.lower()!="cir":
+    if a!="%2" and a.upper() not in ["SR","C","O","ABS","CA","LCM","GCD","CIR"] and a!="+++" and a!="~" and a!="!":
         a_number = int(input("Select a number: "))
         a_number_again = int(input("Select another number: "))
         oh = a_number * a_number_again
@@ -31,7 +31,7 @@ try:
             print(f"{a_number} - {a_number_again} = 0")
 
     elif a=="*":
-        print(f"{a_number} * {a_number_again} = {a_number * a_number_again}") 
+        print(f"{a_number} * {a_number_again} = {a_number * a_number_again}")
 
     elif a=="/":
         if a_number < a_number_again:
@@ -71,6 +71,11 @@ try:
         b = a_number * a_number
         print(f"The square of {a_number} is {b}. The square root of {b} is {int(np.sqrt(b))}")
     
+    elif a.upper()=="CR":
+        a_number = int(input("Select a number to cube: "))
+        cube = float(a_number ** 3)
+        print(f"The cube of {a_number} is {int(cube)}. The cube root of {int(cube)} is {int(np.cbrt(cube))}")
+    
     elif a.upper()=="O":
         a = float(input("Enter a decimal number: "))
         if a >= 0:
@@ -98,7 +103,7 @@ try:
             if abc==-1:
                 liste.pop(-1)
                 AO = int(np.median(liste))
-                print(f"The median of your numbers is {AO}, The float of that number is {float(AO)}")
+                print(f"The median of your numbers is {int(AO)}, The float of that number is {float(AO)}")
                 break
     
     elif a=="!":
@@ -124,22 +129,60 @@ try:
         while True:
             try:    
                 pi = int(input("Enter pi: "))
-                radius = int(input("Enter radius:"))
+                radius = int(input("Enter radius: "))
                 radiussqr = radius * radius
                 print(f"The area of your circle is {int(radiussqr * pi)}")
                 break
             except ValueError:
                 print("Please enter a number.")
     
-    elif a.lower()=="cir":
+    elif a.upper()=="CIR":
         while True:
             try:
                 pi = float(input("Enter pi: "))
-                diameter = float(input("Enter diameter:"))
+                diameter = float(input("Enter diameter: "))
                 print(f"The circumference of your circle is {int(diameter * pi)}, the float of that number is {float(diameter * pi)}")
                 break
             except ValueError:
                 print("Please enter a number.")
+    elif a.upper()=="LCM":
+        liste = []
+        print("You can add values one by one, and every number must be positive.")
+        
+        while True:
+            abc = int(input("Please enter 2+ numbers and type -1 to have your answer: "))
+            liste.append(abc)
+            if abc==-1:
+                liste.pop(-1)
+                if len(liste) < 2:
+                    print("An error has accured. This can be caused by too few numbers, or a number being 0 or smaller.")
+                    break
+                elif len(liste) >= 2:
+                    AO = int(np.lcm.reduce(liste))
+                    print(f"\nThe least common multiple of your numbers is {int(AO)}. The float of that number is {float(AO)}")
+                    break
+            elif abc <= -2 or abc==0:
+                print("Please enter a number that is higher than -1 that is not 0")
+    
+    elif a.upper()=="GCD":
+        liste = []
+        print("You can add values one by one, and every number must be positive.")
+        
+        while True:
+            abc = int(input("Please enter 2+ numbers and type -1 to have your answer: "))
+            liste.append(abc)
+            if abc==-1:
+                liste.pop(-1)
+                if len(liste) < 2:
+                    print("An error has accured. This can be caused by too few numbers, or a number being 0 or smaller.")
+                    break
+                elif len(liste) >= 2:
+                    AO = int(np.gcd.reduce(liste))
+                    print(f"\nThe greatest common divisor of your numbers is {int(AO)}. The float of that number is {float(AO)}")
+                    break
+            elif abc <= -2 or abc==0:
+                print("Please enter a number that is higher than -1 that is not 0")
+    
 
 except ValueError:
     print("An error has accured, please make sure you have typed a number")
