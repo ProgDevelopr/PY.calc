19e514177a0060a521ab8a4e323d28e214853ba8
prog202 <meteturan0706@gmail.com>
2025-07-22 11:31:41 +0300

    Basic Trigonometry

--- calculator.py
+++ calculator.py
@@ -3,24 +3,23 @@ import math as mt
 
 try:
     print("Made by PyDev")
-    
     print("+ = Addition\n- = Subtraction\n* = Multiplication\n/ = Division\n//% = Floor division with remainder\n** = Exponents")
     print("% = Percentages\n%2 = Even or odd\n-- = Subtraction with negative numbers\nSR = Square and square root")
     print("CR = Cube and cube root\nO = Rounding\n+++ = Artihmetic addition\n~ = Median\n! = Factorial\nABS = Absolute value")
     print("CA = Circle Area\nCir = Circumference of the circle\nLCM = Least common multiple\nGCD = Greatest common divisor")
+    print("S = Sign\nLOG = Logarithm\nSIN = Sine\nCOS = Cosine\nTAN = Tangent\nPT = Pythagorean theorem")
     
-    a = input("Please select a symbol: (+,-,*,/,//%,**,%,%2,--,SR,CR,O,+++,~,!,ABS,CA,Cir,LCM,GCD) ")
+    a = input("Please select a symbol: (+,-,*,/,//%,**,%,%2,--,SR,CR,O,+++,~,!,ABS,CA,Cir,LCM,GCD,S,LOG,SIN,COS,TAN,PT) ")
     
-    if a!="%2" and a.upper() not in ["SR","C","O","ABS","CA","LCM","GCD","CIR"] and a!="+++" and a!="~" and a!="!":
-        a_number = int(input("Select a number: "))
-        a_number_again = int(input("Select another number: "))
+    if a not in ["%2","+++","!","~","%","**"] and a.upper() not in ["SR","CR","O","ABS","CA","LCM", \
+    "GCD","CIR","S","LOG","SIN","COS","TAN","PT"]:
+        
+        a_number = float(input("Select a number: "))
+        a_number_again = float(input("Select another number: "))
         oh = a_number * a_number_again
-    
-    elif a=="%2":
-        evenorodd = int(input("Select a number: "))
 
     if a=="+":
-       print(f"{a_number} + {a_number_again} = {a_number + a_number_again}") 
+       print(f"{float(a_number)} + {float(a_number_again)} = {a_number + a_number_again}") 
 
     elif a=="-":
         if a_number < a_number_again:
@@ -31,7 +30,7 @@ try:
             print(f"{a_number} - {a_number_again} = 0")
 
     elif a=="*":
-        print(f"{a_number} * {a_number_again} = {a_number * a_number_again}")
+        print(f"{float(a_number)} * {a_number_again} = {a_number * a_number_again}")
 
     elif a=="/":
         if a_number < a_number_again:
@@ -52,12 +51,18 @@ try:
     
     
     elif a=="**":
-        print(f"{a_number} ** {a_number_again} = {a_number ** a_number_again}")
+        a_number = int(input("Enter a number: "))
+        power = int(input("Enter the power: "))
+        print(f"{a_number} ** {power} = {a_number ** power}")
     
     elif a=="%":
-        print(f"The {a_number} of {a_number_again}% is {oh/100}%")
+        a_percentage = int(input("Enter the percentage: "))
+        a_number = int(input("Enter a number: "))
+        perpro = a_number * a_percentage
+        print(f"The {a_percentage}% of {a_number} is {perpro/100}")
     
     elif a=="%2":
+        evenorodd = int(input("Select a number: "))
         if evenorodd%2==0:
             print(f"{evenorodd} is an even number")
         elif not evenorodd%2==0:
@@ -69,12 +74,12 @@ try:
     elif a.upper()=="SR":
         a_number = int(input("Select a number to square: "))
         b = a_number * a_number
-        print(f"The square of {a_number} is {b}. The square root of {b} is {int(np.sqrt(b))}")
+        print(f"The square of {a_number} is {b}. The square root of {a_number} is {int(np.sqrt(a_number))}")
     
     elif a.upper()=="CR":
         a_number = int(input("Select a number to cube: "))
         cube = float(a_number ** 3)
-        print(f"The cube of {a_number} is {int(cube)}. The cube root of {int(cube)} is {int(np.cbrt(cube))}")
+        print(f"The cube of {a_number} is {int(cube)}. The cube root of {a_number} is {int(np.cbrt(a_number))}")
     
     elif a.upper()=="O":
         a = float(input("Enter a decimal number: "))
@@ -102,7 +107,7 @@ try:
             liste.append(abc)
             if abc==-1:
                 liste.pop(-1)
-                AO = int(np.median(liste))
+                AO = np.median(liste)
                 print(f"The median of your numbers is {int(AO)}, The float of that number is {float(AO)}")
                 break
     
@@ -113,14 +118,14 @@ try:
                 print("Sorry, but you can't use 0 in factorial calculations")
             elif factnum!=0:
                 fact = mt.factorial(factnum)
-                print(f"The factorial of {factnum} is {fact}")
+                print(f"{factnum}! = {fact}")
                 break
     
     elif a.upper()=="ABS":
         while True:
             try:    
                 a_number = int(input("Enter a number: "))
-                print(f"The absolute value of {a_number} is {abs(a_number)}")
+                print(f"|{a_number}| = {abs(a_number)}")
                 break
             except ValueError:
                 print("Please enter a number.")
@@ -131,7 +136,7 @@ try:
                 pi = int(input("Enter pi: "))
                 radius = int(input("Enter radius: "))
                 radiussqr = radius * radius
-                print(f"The area of your circle is {int(radiussqr * pi)}")
+                print(f"The area of your circle is {int(radiussqr * pi)}. That number in float is {float(radiussqr * pi)}")
                 break
             except ValueError:
                 print("Please enter a number.")
@@ -141,7 +146,7 @@ try:
             try:
                 pi = float(input("Enter pi: "))
                 diameter = float(input("Enter diameter: "))
-                print(f"The circumference of your circle is {int(diameter * pi)}, the float of that number is {float(diameter * pi)}")
+                print(f"The circumference of your circle is {int(diameter * pi)}. the float of that number is {float(diameter * pi)}")
                 break
             except ValueError:
                 print("Please enter a number.")
@@ -166,7 +171,7 @@ try:
     
     elif a.upper()=="GCD":
         liste = []
-        print("You can add values one by one, and every number must be positive.")
+        print("You can add values one by one, and every number must be positive!")
         
         while True:
             abc = int(input("Please enter 2+ numbers and type -1 to have your answer: "))
@@ -183,6 +188,75 @@ try:
             elif abc <= -2 or abc==0:
                 print("Please enter a number that is higher than -1 that is not 0")
     
+    elif a.upper()=="S":
+        num = float(input("Type a number: "))
+        if num > 0:
+            print(f"The sign of {num} is 1")
+        elif num < 0:
+            print(f"The sign of {num} is -1")
+        elif num == 0:
+            print(f"The sign of {num} is 0")
+        else:
+            print("An error has accured. Please choose a number.")
+    
+    elif a.upper()=="LOG":
+        print("Make sure you enter positive numbers (Especially for base number)!")
+        base = float(input("Type base number: "))
+        if base <= 0 or base == 1:
+            print("An error has accured. Base too small and can not be 1.")
+        elif base > 0:
+            des = float(input("Type the destination: "))
+            if des <= 0:
+                print("An error has accured. Destination too small.")
+            elif base > 0:
+                num = mt.log(des,base)
+                print(f"{base} must be raised to the power of {num:.2f} to get {des}")
+    
+    elif a.upper()=="SIN":
+        opp = float(input("Enter the opposite edge: "))
+        hyp = float(input("Enter the hypotenuse: "))
+        print(f"The sin of {opp} and {hyp} is {opp/hyp:.2f}")
+        
+    elif a.upper()=="COS":
+        adj = float(input("Enter the adjacent edge: "))
+        hyp = float(input("Enter the hypotenuse: "))
+        print(f"The sin of {adj} and {hyp} is {adj/hyp:.2f}")
+    
+    elif a.upper()=="TAN":
+        adj = float(input("Enter the adjacent edge: "))
+        opp = float(input("Enter the opposite edge: "))
+        print(f"The sin of {opp} and {adj} is {opp/adj:.2f}")
+
+    elif a.upper()=="PT":
+        HYPorEdge = str(input("Would you like to calculate the hypotenuse or an edge? (HYP,EDGE): "))
+        
+        if HYPorEdge.upper()=="HYP":
+            AnEdge = float(input("Enter the value of an edge: "))
+            AnotherEdge = float(input("Enter the value of another edge: "))
+            def HYPcal(a,b):
+                a *= a
+                b *= b
+                c = a + b
+                answer = np.sqrt(c)
+                print(f"The hypotenuse of {a} and {b} is {answer:.2f}")
+            HYPcal(AnEdge,AnotherEdge)
+        
+        elif HYPorEdge.upper()=="EDGE":
+            AnEdge = float(input("Enter the value of an edge: "))
+            hyp = float(input("Enter the value of hypotenuse: "))
+            def EDGEcal(a,b):
+                a *= a
+                b *= b
+                c = b - a
+                answer = np.sqrt(c)
+                print(f"The other edge of {b} (hypotenuse) and {a} is {answer:.2f}")
+            EDGEcal(AnEdge,hyp)
+
+
+            
+    
+    else:
+        print("Please choose a mode.")
 
 except ValueError:
     print("An error has accured, please make sure you have typed a number")
